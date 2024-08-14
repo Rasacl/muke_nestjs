dde880be5544cdd386af9918fbcb36ae3f8fb3a1
chenxiao <chenxiao@uino.com>
2024-08-14 18:35:33 +0800

    feat: 接口

--- nestjs-demo/src/user/user.controller.ts
+++ nestjs-demo/src/user/user.controller.ts
@@ -1,24 +1,30 @@
 import { Controller, Get, Post } from '@nestjs/common'
 import { UserService } from './user.service'
 import { ConfigService } from '@nestjs/config'
+import { User } from './user.entity'
 // import { ConfigEnum } from '../enum/config.enum'
 @Controller('user')
 export class UserController {
   constructor(
-    private readonly userService: UserService,
-    private readonly configService: ConfigService
+    private readonly userService: UserService
+    // private readonly configService: ConfigService
   ) {} // 等价于 this.userService = new UserService()
   @Get()
   getUsers(): any {
-    // const db = this.configService.get(ConfigEnum.DB)
-    // const host = this.configService.get(ConfigEnum.DB_HOST)
-    const data = this.configService.get('db')
-    console.log(data, 11111)
-    return this.userService.getUsers()
+    return this.userService.findAll()
   }
 
   @Post()
   addUser() {
-    return this.userService.addUser()
+    const user = {
+      username: 'Timo',
+      password: '123456'
+    } as User
+    return this.userService.create(user)
+  }
+
+  @Get('/logs')
+  getUserLogs(id: number) {
+    return this.userService.findLogs(1)
   }
 }
--- nestjs-demo/src/user/user.service.ts
+++ nestjs-demo/src/user/user.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from './user.entity'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class UserService {
-  getUsers(): any {
-    return {
-      code: 0,
-      data: [111],
-      msg: '请求用户成功'
-    }
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>
+  ) {}
+
+  findAll() {
+    return this.userRepository.find()
+  }
+  find(username: string): any {
+    return this.userRepository.findOne({ where: { username } })
+  }
+  create(user: User): any {
+    const userTmp = this.userRepository.create(user)
+    return this.userRepository.save(userTmp)
+  }
+  update(id: number, user: Partial<User>): any {
+    return this.userRepository.update(id, user)
+  }
+  remove(id: number) {
+    return this.userRepository.delete(id)
   }
 
-  addUser(): any {
-    return {
-      code: 0,
-      data: [111],
-      msg: '用户添加成功'
-    }
+  findLogs(id: number): any {
+    return this.userRepository.findOne({
+      where: { id },
+      relations: {
+        logs: true
+      }
+    })
   }
 }
